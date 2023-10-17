f3cde862894e76e755dd878fd0f03ec575410b23
srdjanf <srdjan.filipovic98@gmail.com>
2023-10-17 15:28:14 +0200

    Checking for exsisting user. Sends ID to frontend

--- login.py
+++ login.py
@@ -31,22 +31,29 @@ async def login(user: User):
     user_id = authenticate_user(user.username, user.password)
     if not user_id:
         return {"status": "error", "detail": "Invalid credentials.", "redirect": None}
-    return {"status": "success", "detail": "Logged in successfully", "redirect": "/imageDetails/page"}
+    return {"status": "success", "detail": "Logged in successfully", "redirect": "/imageDetails/page", "id": user_id}
 
 
 @router.get("/guest/")
 def guest():
-    return {"status": "success", "detail": "Logged in as guest", "redirect": "/imageDetails/page"}
+    return {"status": "success", "detail": "Logged in as guest", "redirect": "/imageDetails/page", "id": "Guest"}
 
 
 @router.post("/new/")
 async def create_user(new_user: NewUser):
-    
     last_user = user_handler.collection.find_one(sort=[("id", DESCENDING)])
     new_id = last_user["id"] + 1
+
+    existing_user = get_user(new_user.username)
+    if existing_user:
+        return {"status": "error", "detail": "Username already exists.", "redirect": None}
+    
     user_doc = User(id=new_id, username=new_user.username, password=new_user.password)
     
-    user_handler.collection.insert_one(user_doc.model_dump())
-    
-    return {"detail": "User created successfully", "redirect": "/imageDetails/page"}
+    try:
+        user_handler.insert_document(user_doc.model_dump())
+        return {"status": "success", "detail": "User created successfully", "id": new_id}
+    except Exception as e:
+        print(f"Error during incerting: {e}")
+        return {"status": "error", "detail": f"Error during user creation: {e}", "redirect": None}
 
